8672576ace550bcac1b87ddd237a3770196f4e7b
CarlosCunha99 <carloseduardopinhodacostacunha@gmail.com>
2021-04-21 16:25:33 +0100

    First commit

---
+++ data.js
@@ -0,0 +1,9 @@
+var today = new Date();
+var date = today.getFullYear()+ "/" 
+        + (today.getMonth() + 1) + "/" 
+        + today.getDate() + " " 
+        + today.getHours() + ":" 
+        + today.getMinutes() + ":"
+        + today.getSeconds();
+
+
