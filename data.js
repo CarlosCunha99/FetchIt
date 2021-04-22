a2310ab44d6384b4e6f528a499c2fd2ccdc237b0
Chokopikku <fpprofissional@gmail.com>
2021-04-22 16:40:05 +0100

    Pikku test commit

--- data.js
+++
@@ -1,7 +0,0 @@
-var today = new Date();
-var date = today.getFullYear()+ "/" 
-        + (today.getMonth() + 1) + "/" 
-        + today.getDate() + " " 
-        + today.getHours() + ":" 
-        + today.getMinutes() + ":"
-        + today.getSeconds();
